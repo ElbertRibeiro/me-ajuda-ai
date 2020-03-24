45a5be6932b636e459617f282f97b8882374f6a3
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-03-24 09:51:57 -0300

    update

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+/*
+    GET: listar/buscar uma informação no backend
+    POST: Criar uma informação no backend
+    PUT: ALterar uma informação
+    DELETE: Deletar uma informação
+*/
+
+/**
+ 
+    Tipos de parametros:
+
+    Query: Parametors enviados na rota
+           após "?" (filros, paginação)
+    
+    Route:  Parametros ultiliados para 
+            indentificar recursos
+
+ */
+ 
+app.get('/users/:id', (request, response) => {
+    const params = request.params;
+    console.log(params);
+
     return response.json({
         evento: 'Semana Oministack'
     });
