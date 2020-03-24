914e564edb0cd30d42afc444a6bc748f21d38aef
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-03-24 11:23:59 -0300

    up

--- backend/src/database/migrations/20200324111227_creat_ongs.js
+++ backend/src/database/migrations/20200324111227_creat_ongs.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+    knex.schema.createTable('ongs', function (table) {
+        table.string('id').primary();
+        table.string('name').notNulllabel();
+        table.string('email').notNulllabel();
+        table.string('whatsapp').notNulllabel();
+        table.string('city').notNulllabel();
+        table.string('uf', 2).notNulllabel();
+    });
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTable('ongs');
 };
